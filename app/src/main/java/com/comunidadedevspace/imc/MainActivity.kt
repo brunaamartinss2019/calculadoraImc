8ef8e3c7bd7c095f9c5fb5c3a5b902cf0285991e
bruna <brunaa.martins@gmail.com>
2024-05-30 18:10:15 +0200

    Layout segunda tela Passar resultados para segunda tela

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.comunidadedevspace.imc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -10,12 +11,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        //Recuperar os componentes EditText
-            //Criar uma variavel e associar (sinal de =) o component UI <TextInputEditText>
-        //Recuperar o botao da tela
-        //colocar acao no botao setOnClickListener
-        //Recuperar o texto digitado no edtPeso
 
         val edtPeso = findViewById<TextInputEditText>(R.id.edt_peso)
         val edtAltura = findViewById<TextInputEditText>(R.id.edt_altura)
@@ -28,7 +23,8 @@ class MainActivity : AppCompatActivity() {
             if (pesoStr == "" || alturaStr == ""){
             //Mostrar mensagem pro usuario
                 Snackbar.make(
-                    edtPeso, 1.60"Preencha todos os campos",
+                    edtPeso,
+                    "Preencha todos os campos",
                     Snackbar.LENGTH_LONG
                 ).show() //o .show é pra mostrar o snack bar com a mensagem pro usuario
             } else{
@@ -37,6 +33,17 @@ class MainActivity : AppCompatActivity() {
 
                 val alturaQ2 = altura * altura
                 val resultado = peso / alturaQ2
+
+                //Navegar para proxima tela
+                //Criar o layout da proxima tela
+                //Passar dados (resultado) para proxima tela
+
+                //Intent - Classe do proprio Android
+                // (Serve para declarar a intençao de fazer alguma coisa no sistema operacional do Android)
+
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.putExtra(KEY_RESULT_IMC, resultado)
+                startActivity(intent)
                 println("Bruna acao do botao" + resultado)
             }
 
